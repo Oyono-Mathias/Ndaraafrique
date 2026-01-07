b0fffeb1ce1b132ba26f8f217c2caacdc9117cf4
Forma Afrique <formaafrique@gmail.com>
2026-01-07 07:25:04 +0000

    Le message dans le bandeau devrai circuler quand c'est long

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -94,10 +94,15 @@ export default {
             height: '0',
           },
         },
+        'marquee-fast': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'marquee-fast': 'marquee-fast 20s linear infinite',
       },
     },
   },
