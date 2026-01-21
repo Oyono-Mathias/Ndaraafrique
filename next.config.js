ac2bc3807735e8cc9409d3a961030bb708d7b1f9
Forma Afrique <formaafrique@gmail.com>
2026-01-21 06:02:51 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Couldn't find ne

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const withNextIntl = require('next-intl/plugin')(
+  './src/i18n.ts'
+);
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configuration options for Next.js can be placed here.
 };
  
-module.exports = nextConfig;
+module.exports = withNextIntl(nextConfig);
--- src/middleware.ts
+++ src/middleware.ts
@@ -10,16 +10,5 @@ export default createMiddleware({
  
 export const config = {
   // Match only internationalized pathnames
-  matcher: [
-    // Enable a redirect to a matching locale at the root
-    '/',
-
-    // Set a cookie to remember the previous locale for
-    // all requests that have a locale prefix
-    '/(fr|en)/:path*',
-
-    // Enable redirects that add a locale prefix
-    // (e.g. `/pathnames` -> `/en/pathnames`)
-    '/((?!_next|.*\\..*).*)'
-  ]
+  matcher: ['/', '/(fr|en)/:path*']
 };
