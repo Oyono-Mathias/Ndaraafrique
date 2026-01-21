ca694f3b050cce0c6407653b9897cbfea04d37f0
Forma Afrique <formaafrique@gmail.com>
2026-01-21 05:12:07 +0000

    1 of 1 error Next.js (14.2.35) is outdated (learn more)
    Unhandled Runti

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
-const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configuration options for Next.js can be placed here.
+  // The internationalization (i18n) setup is now handled entirely by `src/middleware.ts`.
+};
  
-module.exports = withNextIntl({
-  // Other Next.js configuration ...
-});
+module.exports = nextConfig;
