f66d5145f652eeed019fc4fc685964324638d1bd
Forma Afrique <formaafrique@gmail.com>
2026-02-07 01:10:17 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:869467208668:web:e137d81578dce4b7ebad19",
   "apiKey": "AIzaSyD1u7V-XApA4sQZGZHOJGG9977BUfGAq4A",
   "authDomain": "studio-8433788818-3228d.firebaseapp.com",
-  "storageBucket": "studio-8433788818-3228d.appspot.com",
+  "storageBucket": "studio-8433788818-3228d.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "869467208668"
 };
