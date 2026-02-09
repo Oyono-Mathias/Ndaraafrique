beaec5ec35ebd4f140977b04885f3cdedfe04620
Forma Afrique <formaafrique@gmail.com>
2026-02-09 19:13:45 +0000

    Mais est ce que le app est connecter a la basse de donne ?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-8433788818-3228d",
-  "appId": "1:869467208668:web:e137d81578dce4b7ebad19",
-  "apiKey": "AIzaSyD1u7V-XApA4sQZGZHOJGG9977BUfGAq4A",
-  "authDomain": "studio-8433788818-3228d.firebaseapp.com",
-  "storageBucket": "studio-8433788818-3228d.firebasestorage.app",
-  "measurementId": "",
-  "messagingSenderId": "869467208668"
+  apiKey: "AIzaSyD1u7V-XApA4sQZGZHOJGG9977BUfGAq4A",
+  authDomain: "studio-8433788818-3228d.firebaseapp.com",
+  projectId: "studio-8433788818-3228d",
+  storageBucket: "studio-8433788818-3228d.firebasestorage.app",
+  messagingSenderId: "869467208668",
+  appId: "1:869467208668:web:e137d81578dce4b7ebad19"
 };
